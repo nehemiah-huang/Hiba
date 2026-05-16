d48b663a5663c6889a7f24d63a0e744247245d80
nehemiah-huang <huangnehemiah10@gmail.com>
2026-05-16 13:29:50 +0000

    RU

--- javascript/script.js
+++ javascript/script.js
@@ -523,7 +523,7 @@ const testimonials = {
             initials: 'AA',
             name: 'Awurabena Asante',
             tag: 'Client Experience',
-            sigRole: `Customer  ${'<br><br>'} buro. | Shared workspaces and offices`,
+            sigRole: `Customer  ${'<br>'} buro. | Shared workspaces and offices`,
             date: 'April 12, 2026',
             img: 'images/ama.jpg',
             message: `Good evening buro, just sending massive thanks for your support yesterday. 
