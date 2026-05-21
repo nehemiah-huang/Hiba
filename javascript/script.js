68c585b648a69a212d0d78392d06349580db7459
nehemiah-huang <huangnehemiah10@gmail.com>
2026-05-21 13:00:37 +0000

    Testing

--- javascript/script.js
+++ javascript/script.js
@@ -599,12 +599,12 @@ const testimonials = {
             message: `Made a couple of comments. almost perfect! Great job! This is the best anyone has done so far. 👏`
         },
         michael: {
-            initials: 'MI',
-            name: 'Michael',
+            initials: 'PK',
+            name: 'Patrick Aku Klutse',
             tag: 'Ops delegation',
-            date: 'March 20, 2026',
+            date: 'May 21, 2026',
             img: 'images/michael.jpg',
-            message: `Feels like a weight has been lifted. I'd been carrying too many things for too long and telling myself it was just what running a business looked like. The delegation framework you mapped out wasn't complicated but I needed someone to actually show me how to use it with my specific team. My Monday mornings look completely different now. I can think again.`
+            message: `Team buro.! 🇬🇭 Hope you are all having a great week./n/n/nI am very happy to announce our newest Employee of the Month. For this round, the recognition goes to Hiba! 🏆 Your growth recently and the ownership you have taken over your tasks has been truly remarkable. The challenge ahead is to maintain and build on this standard, but we have no doubt you can do it. Mo ne yɔ, Hiba! 🎊`
         },
         daniel: {
             initials: 'DA',
