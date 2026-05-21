80d839952603cb5a164762dd8e33629ff571c9e9
nehemiah-huang <huangnehemiah10@gmail.com>
2026-05-21 13:10:38 +0000

    Navbar fixed

--- javascript/script.js
+++ javascript/script.js
@@ -602,9 +602,11 @@ const testimonials = {
             initials: 'PK',
             name: 'Patrick Aku Klutse',
             tag: 'Ops delegation',
+            sigName: `Founder & Director`,
+            sigRole: `<span><br></span>buro. | Shared workspaces and offices`,
             date: 'May 21, 2026',
             img: 'images/michael.jpg',
-            message: `Team buro.! 🇬🇭 Hope you are all having a great week./n/n/nI am very happy to announce our newest Employee of the Month. For this round, the recognition goes to Hiba! 🏆 Your growth recently and the ownership you have taken over your tasks has been truly remarkable. The challenge ahead is to maintain and build on this standard, but we have no doubt you can do it. Mo ne yɔ, Hiba! 🎊`
+            message: `Team buro.! 🇬🇭 Hope you are all having a great week.<span><br></span>I am very happy to announce our newest Employee of the Month. For this round, the recognition goes to Hiba! 🏆 Your growth recently and the ownership you have taken over your tasks has been truly remarkable. The challenge ahead is to maintain and build on this standard, but we have no doubt you can do it. Mo ne yɔ, Hiba! 🎊`
         },
         daniel: {
             initials: 'DA',
